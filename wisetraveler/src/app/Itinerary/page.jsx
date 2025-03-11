029be6634afda1abd2651bd7fd23115be6a80dd8
Dh025 <115023967+Dh025@users.noreply.github.com>
2025-03-10 23:36:22 -0400

    Add files via upload

--- wisetraveler/src/app/Itinerary/page.jsx
+++ wisetraveler/src/app/Itinerary/page.jsx
@@ -51,164 +51,212 @@ export default function Itinerary()
   return (
     <div>
       <h1 className={styles.header}>Customize Your Travel Preferences</h1>
-      
-      <form
-        className={styles.form}>
-        <label className={styles.label}>
-          Preferred Destination
-          <input
-            id="destination"
-            type="destination"
-            name="destination"
-            placeholder="Enter Destination..."
-            className={styles.input}
-            required
-            onChange={(e) => setDestination(e.target.value)}
-          />
-        </label>
+      <div>
+        <form
+          className={styles.form}>
+          <label className={styles.label}>
+            Preferred Destination
+            <input
+              id="destination"
+              type="destination"
+              name="destination"
+              placeholder="Enter Destination..."
+              className={styles.input}
+              required
+              onChange={(e) => setDestination(e.target.value)}
+            />
+          </label>
 
-        <label className={styles.label}>
-          Type of Trip
-          <button type="button" className={styles.TripButton}> 
-            Solo Trip
-          </button>
+          <label className={styles.label}>
+            Type of Trip
+          </label>
 
-          <button type="button" className={styles.TripButton}> 
-            Couples Trip
-          </button>
+          <div style={{display: "flex"}}>
+            
+            <button type="button" className={styles.TripButton}> 
+              Solo Trip
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Friends Trip
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Couples Trip
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Family Vacation
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Friends Trip
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Company Retreat
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Family Vacation
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Event
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Company Retreat
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Other
-          </button>
-        </label>
+            <button type="button" className={styles.TripButton}> 
+              Event
+            </button>
 
-        <label className={styles.label}>
-          Preferred Activities
-          <button type="button" className={styles.TripButton}> 
-            Local Food
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Other
+            </button>
+          </div>
 
-          <button type="button" className={styles.TripButton}> 
-            Luxury
-          </button>
+          <label className={styles.label}>
+            Preferred Activities
+          </label>
 
-          <button type="button" className={styles.TripButton}> 
-           Outdoors
-          </button>
+          <div style={{display: "flex"}}>
 
-          <button type="button" className={styles.TripButton}> 
-            Popular Attractions
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Local Food
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Nightlife
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Luxury
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Wildlife
-          </button>
+            <button type="button" className={styles.TripButton}> 
+            Outdoors
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Art
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Popular Attractions
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Beach
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Nightlife
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            History
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Wildlife
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Wellness
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Art
+            </button>
+          </div>
 
-          <button type="button" className={styles.TripButton}> 
-            Placeholder
-          </button>
+          <div style={{display: "flex"}}>
 
-          <button type="button" className={styles.TripButton}> 
-            Wine
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              Beach
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Museums
-          </button>
+            <button type="button" className={styles.TripButton}> 
+              History
+            </button>
 
-          <button type="button" className={styles.TripButton}> 
-            Adventure/Sports
-          </button>
-        </label>
- 
-      </form>
+            <button type="button" className={styles.TripButton}> 
+              Wellness
+            </button>
 
-     {/*<Image
-        src={"/images/istockphoto-1526986072-612x612.jpg"}
-        alt="Itinerary"
-        width={612}
-        height={612}
-        className={styles.Image}
-      />*/}
+            <button type="button" className={styles.TripButton}> 
+              Placeholder
+            </button>
+
+            <button type="button" className={styles.TripButton}> 
+              Wine
+            </button>
+
+            <button type="button" className={styles.TripButton}> 
+              Museums
+            </button>
+
+            <button type="button" className={styles.TripButton}> 
+              Adventure/Sports
+            </button>
+          </div>
+        </form>
+      </div>
 
       <h1 className={styles.header}>Your Personalized Travel Recommendations</h1>
+      <form className={styles.form}>
+        <div style={{display: "flex", justifyContent: "center", textAlign:"center"}}>
+          <div>
+            <Image
+              src={"/images/Greece.webp"}
+              alt="Itinerary"
+              width={400}
+              height={400}
+              className={styles.Image}
+            />
+            <h2>Greece</h2>
+          </div>
+          
+          <div>
+            <Image
+              src={"/images/paris.jpg"}
+              alt="Itinerary"
+              width={400}
+              height={400}
+              className={styles.Image}
+            />
+            <h2>Paris</h2>
+          </div>
 
-      <form 
-        className={styles.form}>
-        
+          <div>
+            <Image
+              src={"/images/seoul.jpg"}
+              alt="Itinerary"
+              width={400}
+              height={400}
+              className={styles.Image}
+            />
+            <h2>Seoul</h2>
+          </div>
+        </div>
       </form>
 
       <h1 className={styles.header}>Translation Tool</h1>
-      <form
-        className={styles.form}>
-        <label className={styles.label}>
-          Select Language To Translate From <button type="button" className={styles.TripButton}>
-            Language
-            </button>
+      <form className={styles.form}>
+        <div style={{display: "flex"}}>
+
+          <label className={styles.label}>
+            Select Language To Translate From 
+
+            <select
+              name="selectedLang" 
+              defaultValue="English"
+              className={styles.LangButton}>
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+
             <input
               id = "language"
               type="language"
               name="language"
               placeholder="Enter Text..."
-              className={styles.input}
+              className={styles.input2}
               required
               onChange={(e) => SetLanguage(e.target.value)}
-            />
-        </label>
+              />
+          </label>
 
-        <label className={styles.label}>
-          Select Language To Translate To <button type="button" className={styles.TripButton}>
-            Language
-            </button>
+          <label className={styles.label}>
+            Select Language To Translate To 
+
+            <select 
+              name="selectedLang" 
+              defaultValue="Spanish"
+              className={styles.LangButton}>
+              <option value="English">English</option>
+              <option value="Spanish">Spanish</option>
+            </select>
+
             <input
               id = "language"
               type="language"
               name="language"
               placeholder="Translation"
-              className={styles.input}
+              className={styles.input2}
               required
               onChange={(e) => SetLanguage(e.target.value)}
             />
-        </label>
+          </label>
+        </div>
       </form>
-      
-      
     </div>
   );
 }
